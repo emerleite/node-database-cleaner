ea045cf2dbd2de07ad7f6e18c749bcbc9e851324
Emerson Macedo <emerleite@gmail.com>
2011-02-09 02:36:20 -0200

    Delete all collections items. Also a first test structure

---
+++ test/database-cleaner.test.js
@@ -0,0 +1,40 @@
+var testCase = require('nodeunit').testCase,
+    databaseCleaner = require('lib/database-cleaner');
+var mongoose = require('mongoose');
+
+module.exports = testCase({
+  setUp: function (callback) {
+    this.connection = mongoose.createConnection('mongodb://localhost/database_cleaner');
+    this.connection.db.createCollection("database_cleaner_collection", null, function (err, collection) {
+      collection.insertAll([{a:1}, {b:2}], function() {
+        callback();
+      });
+    });
+  },
+  tearDown: function (callback) {
+    this.connection.close();
+    this.connection = null;
+    callback();
+  },
+  'should delete all collections items': function(test) {
+    var db = this.connection.db;
+    databaseCleaner.clean(db, function () {
+      db.collections( function (skip, collections) {
+        var total_collections = collections.length;
+        collections.forEach(function (collection) {
+          if (collection.collectionName != 'system.indexes') {
+            collection.count({}, function (err, count) {
+              test.equal(count, 0);
+              total_collections--;
+              if (total_collections <= 0) {
+                test.done();
+              }
+            });
+          } else { 
+            total_collections--; 
+          }
+        });
+      });
+    });
+  }
+});
